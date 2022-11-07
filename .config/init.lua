385ee7b6f93028faabf57f3e660dc56e6ba94535
Harsh Vyapari <harshv1741@gmail.com>
2022-11-07 17:09:13 +0530

    neovim config files
    This folder contain my custom neovim config files

---
+++ .config/init.lua
@@ -0,0 +1,3 @@
+require('highlights')
+require('maps')
+require('base')
---
+++ .config/nvim/lua/highlights.lua
@@ -0,0 +1,8 @@
+local set = vim.opt
+
+set.cursorline = true
+set.termguicolors = true
+set.winblend = 0
+set.wildoptions = 'pum'
+set.pumblend = 5
+set.background = 'dark'
