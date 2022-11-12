fee886662741b933bbc35055042f07c4811ac318
Harsh Vyapari <harshv1741@gmail.com>
2022-11-12 11:17:55 +0530

    Adding lsp, autocomplete and syntax highlighting We successfully configured autocompletion, lsp, and syntax highlighting in vim editor

---
+++ .config/nvim/after/plugin/treesitter.rc.lua
@@ -0,0 +1,33 @@
+local status, ts = pcall(require, "nvim-treesitter.configs")
+if (not status) then return end
+
+ts.setup {
+	highlight = {
+		enable = true,
+		disable = {},
+	},
+	indent = {
+		enable = true,
+		disable = {},
+	},
+	ensure_installed = {
+		"tsx",
+		"c",
+		"python",
+		"toml",
+		"php",
+		"json",
+		"yaml",
+		"javascript",
+		"markdown",
+		"css",
+		"html",
+		"lua"
+	},
+	autotag = {
+		enable = true,
+	},
+}
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -13,4 +13,14 @@ packer.startup(function(use)
 		requires = { 'tjdevries/colorbuddy.nvim' }
 	}
 	use 'hoob3rt/lualine.nvim' -- Status line
+	use 'L3MON4D3/LuaSnip' -- Snippet
+	use 'onsails/lspkind-nvim' -- vs code like pictograms
+	use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
+	use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's built-in LSP
+	use 'hrsh7th/nvim-cmp' -- Completion
+	use 'neovim/nvim-lspconfig' -- LSP (Language Server Protocol for IDE
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = ':TSUpdate'
+	}
 end)
