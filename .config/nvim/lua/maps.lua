144a4e669df0cecbbd1a55403d25a7c008151572
Harsh Vyapari <harshv1741@gmail.com>
2022-11-15 17:33:51 +0530

    Update maps.lua
    Adding Cut, Copy and Paste to clipboard in vim

--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -17,15 +17,22 @@ keymap.set('n', '<C-a>', 'gg<S-v>G')
 
 -- New tab
 keymap.set('n', 'te', ':tabedit')
+
 -- Split window
 keymap.set('n', 'ss', ':split<Return><C-w>w')
 keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+
 -- Move window
 keymap.set('n', '<Space>', '<C-w>w')
 keymap.set('', 'sh', '<C-w>h')
 keymap.set('', 'sk', '<C-w>k')
 keymap.set('', 'sj', '<C-w>j')
 keymap.set('', 'sl', '<C-w>l')
+
+-- Copy/Cut/Paste or Delete/Yank/Paste to Clipboard
+keymap.set('', '<C-x>', '"+d')
+keymap.set('', '<C-c>', '"+y')
+keymap.set('', '<C-p>', '"+p')
 
 -- Resize window
 keymap.set('n', '<C-w><left>', '<C-w><')
